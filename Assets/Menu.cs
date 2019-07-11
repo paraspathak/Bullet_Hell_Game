18a4d4f86b52009dc9feb8e7d1f60ada17e93944
Paras Pathak <paraspathak@outlook.com>
2019-07-10 21:28:21 -0500

    Menu Built and Firebase Setup

---
+++ Assets/Menu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    //Quit click handler
+    public void onQuitClicked()
+    {
+        Debug.Log("Quit click");
+        Application.Quit();
+    }
+
+    public void onPlayClicked()
+    {
+        Debug.Log("PLAY CLICKED");
+    }
+
+    public void onMusicSliderChange()
+    {
+        //Change Volume here
+    }
+
+    public void onVolumeSliderChange()
+    {
+
+    }
+}
---
+++ Assets/authenticate_user.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class authenticate_user : MonoBehaviour
+{
+    public GameObject create_user;
+    public InputField inputField;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        create_user = GameObject.Find("CreateUserPanel");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AuthenticateUser()
+    {
+        Debug.Log("Authenticate user here");
+    }
+}
