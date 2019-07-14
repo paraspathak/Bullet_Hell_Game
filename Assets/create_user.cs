f1165e7688e9f82fed2eae8391fc53ec1609afa4
Paras Pathak <paraspathak@outlook.com>
2019-07-14 15:23:26 -0500

    Framework loads data from database

--- Assets/create_user.cs
+++ Assets/create_user.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+//Database
 using Firebase;
 using Firebase.Database;
 using Firebase.Unity.Editor;
--- Assets/retrieve_leaderboard.cs
+++ Assets/retrieve_leaderboard.cs
@@ -2,25 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-
-
+using TMPro;
+//Database
+using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System;
+using System.Linq;
 
 public class retrieve_leaderboard : MonoBehaviour
 {
     public GameObject leaderboard_area;
 
-    protected Dictionary<string, Text> map;
+    protected Dictionary<string, TMP_Text> map;
+    protected DatabaseReference database;
 
     // Start is called before the first frame update
     void Start()
     {
         //Add elements onto the map for easy access for updating
-        map = new Dictionary<string, Text>();
-        Text[] table_entry = leaderboard_area.GetComponentsInChildren<Text>();
-        foreach (Text child in table_entry)
+        map = new Dictionary<string, TMP_Text>();
+        TMP_Text[] table_entry = leaderboard_area.GetComponentsInChildren<TMP_Text>();
+        foreach (TMP_Text child in table_entry)
         {
             map.Add(child.name, child);
         }
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://bullet-hell-game.firebaseio.com/");
+        database = FirebaseDatabase.DefaultInstance.RootReference;
+        load_leaderboard();
     }
 
     // Update is called once per frame
@@ -31,6 +40,33 @@ public class retrieve_leaderboard : MonoBehaviour
 
     public void load_leaderboard()
     {
-        
+        //Access the leaderboard region of the database        
+        database.Child("leaderboard").GetValueAsync().ContinueWith(task=> {
+            if (task.IsFaulted)
+            {
+                Debug.Log("Cannot access database");
+                update_error();
+            }
+            else if (task.IsCompleted)
+            {
+                update_leaderboard(task.Result);
+            }
+        });
     }
+
+    public void update_leaderboard(DataSnapshot snapshot)
+    {
+        foreach (DataSnapshot leaderboard_entry in snapshot.Children)
+        {
+            IDictionary dictUser = (IDictionary)leaderboard_entry.Value;
+            Debug.Log("" + dictUser["username"] + " - " + dictUser["score"]);
+        }
+
+    }
+
+    public void update_error()
+    {
+
+    }
+
 }
