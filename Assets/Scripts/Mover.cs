d29034bbb02274e0d5659b2cebce36f4163a913d
Paras Pathak <paraspathak@outlook.com>
2019-07-15 17:12:43 -0500

    Initial import settings

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    public Rigidbody shot;
+    public float speed;
+
+    void Start()
+    {
+        shot.velocity  = transform.forward * speed;
+    }
+}
