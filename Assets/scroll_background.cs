13af921204e6951515f859bf5a63ef824c9ad1af
Paras Pathak <paraspathak@outlook.com>
2019-07-16 19:22:20 -0500

    Trying scroll with blender object

---
+++ Assets/scroll_background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scroll_background : MonoBehaviour
+{
+    public float speed;
+    public Renderer background;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        background.material.mainTextureOffset += new Vector2(speed * Time.deltaTime, 0f);
+    }
+}
