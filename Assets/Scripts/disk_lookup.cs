d0797e93497290847f7e230998e19227718826b7
Paras Pathak <paraspathak@outlook.com>
2019-08-04 16:26:48 -0500

    Try-Catch implemented

--- Assets/Scripts/disk_lookup.cs
+++ Assets/Scripts/disk_lookup.cs
@@ -29,33 +29,43 @@ public class disk_lookup : MonoBehaviour
     private int read_data()
     {
         //FileStream fs = File.Create(Application.persistentDataPath + "/user");
-        string[] lines = System.IO.File.ReadAllLines(Application.persistentDataPath + "/user.dat");
-        Debug.Log(lines);
-        if (lines[0].Equals("true"))
+        try
         {
-            authenticate_user.save = true;  //Propagate the save
-            Debug.Log("Username is saved, loading...");
-            string username = lines[1];
-            Debug.Log(username);
-            string email = lines[2];
-            string password = lines[3];
-            Menu.username = username;
-            //Authenticate with firebase here
-            auth = auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
-            auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
-                if (task.IsCompleted)
-                {
-                    Debug.Log("User signed in successfully");
-                }
-            });
-            return 2;
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);   //Load the main menu
+            string[] lines = System.IO.File.ReadAllLines(Application.persistentDataPath + "/user.dat");
+            Debug.Log(lines);
+            if (lines[0].Equals("true"))
+            {
+                authenticate_user.save = true;  //Propagate the save
+                Debug.Log("Username is saved, loading...");
+                string username = lines[1];
+                Debug.Log(username);
+                string email = lines[2];
+                string password = lines[3];
+                Menu.username = username;
+                //Authenticate with firebase here
+                auth = auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+                auth.SignInWithEmailAndPasswordAsync(email, password).ContinueWith(task => {
+                    if (task.IsCompleted)
+                    {
+                        Debug.Log("User signed in successfully");
+                    }
+                });
+                return 2;
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);   //Load the main menu
+            }
+            else
+            {
+                return 1;
+                //Load the next scene
+                //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   //Load the login scene 
+            }
         }
-        else
+        catch(System.Exception e)
         {
+            Debug.Log(e);
+            FileStream fs = File.Create(Application.persistentDataPath + "/user.dat");
             return 1;
-            //Load the next scene
-            //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);   //Load the login scene 
         }
+       
     }
 }
